aa3e93f6b0ac08186b573be69697b1612f1505e8
Zifei Shan <shanzifei@pku.edu.cn>
2014-02-16 23:00:20 -0800

    Moved frontend jades

--- emotif/config/routes.js
+++ emotif/config/routes.js
@@ -1,7 +1,34 @@
+// 'use strict';
+
+// // var middleware = require('./middleware');
+
+// /**
+//  * Application routes
+//  */
+// module.exports = function(app) {
+
+//   // Server API Routes
+//   app.post('/api/users', users.create);
+//   app.put('/api/users', users.changePassword);
+//   app.get('/api/users/me', users.me);
+//   app.get('/api/users/:id', users.show);
+
+//   app.post('/api/session', session.login);
+//   app.del('/api/session', session.logout);
+
+//   // All other routes to use Angular routing in app/scripts/app.js
+//   app.get('/partials/*', index.partials);
+//   app.get('/*', middleware.setUserCookie, index.index);
+// };
+
+
+// ORIGINAL
+
 module.exports = function(app){
 
-	//home route
-	var home = require('../app/controllers/home');
-	app.get('/', home.index);
+  //home route
+  var home = require('../app/controllers/home');
+  app.get('/', home.index);
 
 };
+
