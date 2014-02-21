275e35f7d34b8b71fb167d49bb7e0c5f1c35b5b0
Yuhao Zhang <denzel.zyh@gmail.com>
2014-02-20 21:08:59 -0800

    Add front-end  main.js

---
+++ emotif/public/js/main.js
@@ -0,0 +1,35 @@
+/*
+* Front end logic for main page.
+*/
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+  $('#main-next-button').click(function(e) {
+    console.log('Next button clicked!');
+    var email = $('#main-email-input').val();
+    
+    //Here add some form validation
+    
+    var url_call = '/gatekeeper';
+    var post_body = {
+      'email': email
+    };
+    console.log(post_body);
+
+    $.post(url_call, post_body, function(result) {
+      if(result.exist) {
+        window.location.href = '/login/:' + email;
+      } else {
+        window.location.href = '/signup/:' + email;
+      }
+    });
+  });
+}
