7918cd9b0325c79e7be515d8725f1571137eadda
Zifei Shan <shanzifei@pku.edu.cn>
2014-02-27 02:08:42 -0800

    Minor fix of fblogin logic

--- emotif/public/js/fblogin.js
+++ emotif/public/js/fblogin.js
@@ -2,26 +2,26 @@
 $(document).ready(function() {
   RegisterNavListener();
   
-  // Facebook login code
-  FBCheckLogin(function(data){
-    console.log('FB status:'+data.status);
-    if (data.status == 'connected') {
+  // // Facebook login code
+  // FBCheckLogin(function(data){
+  //   console.log('FB status:'+data.status);
+  //   if (data.status == 'connected') {
 
-      FB.api('/me', function(data){
-        // var fbid = data.id;
-        if (data == undefined) {
-          window.alert('Error when getting user info from facebook. Use manual login.');
-          FBLogout();
-          window.location.href = '/fblogin';
-        }
-        console.log(data);
-        // window.localStorage.setItem('email', data.username);
-        window.location.href = '/select';
+  //     FB.api('/me', function(data){
+  //       // var fbid = data.id;
+  //       if (data == undefined) {
+  //         window.alert('Error when getting user info from facebook. Use manual login.');
+  //         FBLogout();
+  //         window.location.href = '/fblogin';
+  //       }
+  //       console.log(data);
+  //       // window.localStorage.setItem('email', data.username);
+  //       window.location.href = '/select';
 
-      });
+  //     });
 
-    }
-  });
+  //   }
+  // });
 
 
   $('#fblogin-yes-button').click(function(e) {
@@ -29,23 +29,24 @@ $(document).ready(function() {
     FBLogin(function(data){
       console.log('FB button clicked!');
       console.log(data.status);
-        if (data.status == 'connected') {
+      if (data.status === 'connected') {
 
-          FB.api('/me', function(data){
-            // var fbid = data.id;
-            if (data == undefined) {
-              window.alert('Error when getting user info from facebook. Use manual login.');
-              window.location.href = '/fblogin';
-            }
-            else {
-              window.location.href = '/select';
-            }
-          });
-        }
-        else {
-          window.alert('Fail to login to facebook.');
-          window.location.href = '/fblogin';
-        }
+        FB.api('/me', function(data){
+          console.log(data);
+          // var fbid = data.id;
+          if (data == undefined) {
+            window.alert('Error when getting user info from facebook. Use manual login.');
+            window.location.href = '/fblogin';
+          }
+          else {
+            window.location.href = '/select';
+          }
+        });
+      }
+      else {
+        window.alert('Fail to login to facebook.');
+        window.location.href = '/fblogin';
+      }
     });
   });
 
